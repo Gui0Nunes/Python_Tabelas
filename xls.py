9d38a685518050224e21527dbb1801405163eb50
Guilherme Nunes <guillhermmme@gmail.com>
2023-05-30 02:05:52 -0300

    Atualização comentário

--- xls.py
+++ xls.py
@@ -4,6 +4,7 @@ tabela_pessoas = pd.read_excel("planilha.xlsx")
 
 pd.set_option("display.max_columns", None)
 
+# Imprime a tabela de testes
 # print(tabela_pessoas)
 
 qtdPessoas = tabela_pessoas[['Nome']].count()
